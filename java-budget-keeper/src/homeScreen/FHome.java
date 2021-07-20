fdc5ff4f9ad1986b1e9e23e68587b447e983db77
E-Panda <szczepanska.ewa@hotmail.com>
2021-07-21 00:11:36 +0200

    💄 Adding Panels to Nav Menu
    - Added 3 Panels which will be buttons in the near future,
    - Added rigidArea's.

--- java-budget-keeper/src/homeScreen/FHome.java
+++ java-budget-keeper/src/homeScreen/FHome.java
@@ -9,6 +9,9 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.border.LineBorder;
 import javax.swing.JLabel;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.Dimension;
 
 public class FHome extends JFrame {
 
@@ -48,5 +51,25 @@ public class FHome extends JFrame {
 		PnlMenu.setBackground(new Color(37, 107, 80));
 		contentPane.add(PnlMenu);
 		PnlMenu.setLayout(null);
+		
+		JPanel panel_1_2 = new JPanel();
+		panel_1_2.setBounds(10, 373, 236, 64);
+		PnlMenu.add(panel_1_2);
+		
+		Component rigidArea_1 = Box.createRigidArea(new Dimension(20, 20));
+		rigidArea_1.setBounds(10, 309, 236, 64);
+		PnlMenu.add(rigidArea_1);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(10, 244, 236, 64);
+		PnlMenu.add(panel_1);
+		
+		Component rigidArea = Box.createRigidArea(new Dimension(20, 20));
+		rigidArea.setBounds(10, 180, 236, 64);
+		PnlMenu.add(rigidArea);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 116, 236, 64);
+		PnlMenu.add(panel);
 	}
 }
